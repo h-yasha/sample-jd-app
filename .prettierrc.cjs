e5aee72d5205b004789ad6ea450dd870b7401867
h-yasha <husssaindesu@gmail.com>
2023-03-20 18:03:00 +0300

    install and configure prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+/**
+ * @type {import("prettier").Config}
+ * setup to be inline with rome defaults.
+ */
+const config = {
+  tabWidth: 2,
+  useTabs: true,
+  semi: true,
+  printWidth: 80,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  trailingComma: "all",
+};
+
+module.exports = config;
