a6877f1e87912f571841cc77ab2a64b203977e61
h-yasha <husssaindesu@gmail.com>
2023-03-21 17:04:48 +0300

    install and configure fira-sans font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"fira-sans": ["Fira Sans", "sans-serif"],
+			},
+		},
 	},
 	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
