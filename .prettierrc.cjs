2aa744e38a3a4bfea048db1e535a6877b16d7698
h-yasha <husssaindesu@gmail.com>
2023-03-23 15:48:45 +0300

    format files

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,13 +3,13 @@
  * setup to be inline with rome defaults.
  */
 const config = {
-  tabWidth: 2,
-  useTabs: true,
-  semi: true,
-  printWidth: 80,
-  singleQuote: false,
-  quoteProps: "as-needed",
-  trailingComma: "all",
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	printWidth: 80,
+	singleQuote: false,
+	quoteProps: "as-needed",
+	trailingComma: "all",
 };
 
 module.exports = config;
