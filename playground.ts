1a48c1af2a1c86b39cfbe18e6c016ab41d337607
h-yasha <husssaindesu@gmail.com>
2023-04-10 14:32:41 +0300

    chore: setup ts-node for playground

--- playground.ts
+++ playground.ts
@@ -1,3 +1,21 @@
-import { hashPassword } from "~/functions/hashPassword";
+function pageSizeGenerator(itemCount: number) {
+	const items = new Set<number>();
 
-console.log(hashPassword("password"));
+	const places = itemCount.toString().length;
+
+	for (let i = 1; i < places; i++) {
+		const zeros = "0".repeat(i);
+
+		items.add(parseInt(`${1}${zeros}`));
+		items.add(parseInt(`${2}${zeros}`));
+		items.add(parseInt(`${3}${zeros}`));
+		items.add(parseInt(`${4}${zeros}`));
+		items.add(parseInt(`${5}${zeros}`));
+	}
+
+	items.add(itemCount);
+
+	return items;
+}
+
+console.log(pageSizeGenerator(452));
